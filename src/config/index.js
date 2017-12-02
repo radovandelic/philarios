f15fe8a829d9a646d11dd4e16ed1d66cf9d73bad
Radovan Delic <infamouszgbgd@yahoo.com>
2017-12-02 17:52:55 +0100

    imagesearch api

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,5 @@ import d from './db.js';
 
 c.flickr_key = process.env.FLICKR_KEY;
 c.flickr_secret = process.env.FLIRCKR_SECRET;
-
 export const config = c;
 export const db = d;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import api from './api';
-import { config, db } from './config';
+import { config, db } from './config/index';
 
 let app = express();
 app.server = http.createServer(app);
